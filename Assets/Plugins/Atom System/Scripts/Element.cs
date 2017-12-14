1e285edea175f37f4e2d9c77354a9fb294eb1b58
Will Jones <wdj294@gmail.com>
2017-12-13 22:08:41 -0500

    Updates

---
+++ Assets/Plugins/Atom System/Scripts/Element.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Element : MonoBehaviour {
+
+    //Working Combos
+    //5 p 0.2 scale
+    //10 p 0.4 scale
+    //20 p 0.6 scale
+
+    public string elementName;
+    public string elementSymbol;
+
+    public int atomicNumber;
+    public float atomicWeight;
+
+    public int protonCount;
+
+    public int neutronCount;
+}
+
+   
